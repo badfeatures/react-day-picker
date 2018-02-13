30f4204536395307ed747016dff3a6179a01e487
Ivan Wu <ivanchungwu@gmail.com>
2018-02-12 17:21:46 -0800

    Added typescript declaration for DayPickerInput

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,3 +2,8 @@
 
 import DayPicker from './DayPicker';
 export default DayPicker;
+
+declare module 'react-day-picker/DayPickerInput' {
+  import { DayPickerInput } from 'react-day-picker/types/DayPickerInput';
+  export default DayPickerInput;
+}
