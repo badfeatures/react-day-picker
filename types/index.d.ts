7cb68f0bda81c644251e6ddd45479e9e8733012c
Ivan Wu <ivanchungwu@gmail.com>
2018-02-12 17:37:05 -0800

    Change declarations

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,8 @@
 // TypeScript Version: 2.2
-
-import DayPicker from './DayPicker';
-export default DayPicker;
+declare module 'react-day-picker/DayPicker' {
+  import DayPicker from 'react-day-picker/types/DayPicker';
+  export default DayPicker;
+}
 
 declare module 'react-day-picker/DayPickerInput' {
   import { DayPickerInput } from 'react-day-picker/types/DayPickerInput';
